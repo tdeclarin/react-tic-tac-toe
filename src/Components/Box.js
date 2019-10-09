bab114bc87a42a1ac2259188c1a4f135eca690a6
tdeclarin <tdeclarin@chegg.com>
2019-10-09 14:25:43 -0700

    fixes onclick handler error

--- src/Components/Box.js
+++ src/Components/Box.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Box = props => {
   return (
-    <div class="box">
+    <div className="box" onClick={e => props.clickHandler(e)}>
       <h1>{props.value}</h1>
     </div>
   );
