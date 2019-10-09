7b3b29a1ddd8ca35868245277a81de573f753ee8
tdeclarin <tdeclarin@chegg.com>
2019-10-09 14:23:51 -0700

    Passes in props to Board Component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@ import React, { Component } from "react";
 import Board from "./Components/Board";
 
 class App extends Component {
+  state = {
+    boxValue: Array.from({ length: 9 })
+  };
+
+  clickHandler(e) {
+    console.log(e.target);
+  }
+
   render() {
     return (
       <div>
-        <Board />
+        <div className="title">
+          <h1>Tic-Tac-Toe</h1>
+        </div>
+        <Board
+          playerMove={this.state.boxValue}
+          clickHandler={this.clickHandler}
+        />
       </div>
     );
   }
