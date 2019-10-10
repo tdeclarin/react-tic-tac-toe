7c4d673efb78156b23540bae109316f6432656d4
tdeclarin <tdeclarin@chegg.com>
2019-10-10 13:04:03 -0700

    fixes binding bug

--- src/Components/Box.js
+++ src/Components/Box.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Box = props => {
   return (
-    <div className="box" onClick={e => props.clickHandler(e)}>
+    <div className="box" onClick={e => props.clickHandler(props.index)}>
       <h1>{props.value}</h1>
     </div>
   );
