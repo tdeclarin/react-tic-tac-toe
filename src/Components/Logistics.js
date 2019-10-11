e9fd672c4d9327f4f252efb541925bda0eee73f0
tdeclarin <tdeclarin@chegg.com>
2019-10-11 13:23:00 -0700

    Modifies Logistics component

--- src/Components/Logistics.js
+++ src/Components/Logistics.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Logistics = props => {
-  return <h1>Player Turn: {props.currentPlayer}</h1>;
+  return (
+    <div className="logistics">
+      <h1>Player Turn: {props.currentPlayer}</h1>
+      <h1>Winner of the game is : {props.winner}</h1>
+      <button className="new-game-btn" onClick={e => props.newGame()}>
+        Restart
+      </button>
+    </div>
+  );
 };
 
 export default Logistics;
